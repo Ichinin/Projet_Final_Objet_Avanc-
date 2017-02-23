fed185e4e257880e93dff5f15e52403541457dd0
Ichinin <r.suntharasarma@gmail.com>
2017-02-23 11:34:08 -0500

    Création classe compteBancaire avec méthodes de dépôt et retirer somme.

--- src/CompteBancaire.java
+++ src/CompteBancaire.java
@@ -1,4 +1,39 @@
 
 public class CompteBancaire {
 
+	protected String identifiant;
+	protected int somme;
+	
+	CompteBancaire(String id){
+		this.identifiant = id;
+	}
+	
+	void Consulter_compte(){
+		
+		String reponse_compte = "Vous etes sur le comte " + this.identifiant + "\nLa somme disponible sur le compte est " + this.somme;
+		System.out.println(reponse_compte);
+		//return reponse_compte;
+		
+	}
+	
+	void Deposer_argent(int somme){
+		this.somme += somme;
+		
+		System.out.println("Vous disposez maintenant de la sommme de " + this.somme);
+		
+	}
+	
+	void Retirer_somme(int somme){
+		if (somme<this.somme) {
+			this.somme -= somme;
+			System.out.println("Vous etes a découvert de " + this.somme + " sur votre compte !");
+		}else{
+			this.somme -= somme;
+			System.out.println("Vous disposez maintenant de la sommme de " + this.somme);	
+		}
+
+	}
+	
+	
+	
 }
