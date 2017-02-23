5a1361a9c2403b691b22bbd2e6be28de78f05860
Nyemo KOUMADI <Nyemo KOUMADI>
2017-02-23 11:32:06 -0500

    Codage de la classe Client

--- src/Client.java
+++ src/Client.java
@@ -1,4 +1,46 @@
 
 public class Client {
+	
+	protected String codeClient;
+	protected String nomClient;
+	protected CompteBancaire compte;
+	
+	public Client(String codeClient, String nomClient) {
+		this.codeClient = codeClient;
+		this.nomClient = nomClient;
+	}
+
+	public String getCodeClient() {
+		return codeClient;
+		
+	}
+
+	public void setCodeClient(String codeClient) {
+		this.codeClient = codeClient;
+	}
+
+	public String getNomClient() {
+		return nomClient;
+	}
+
+	public void setNomClient(String nomClient) {
+		this.nomClient = nomClient;
+	}
+
+	public CompteBancaire getCompte() {
+		return compte;
+	}
+
+	public void setCompte(CompteBancaire compte) {
+		this.compte = compte;
+	}
+
+	@Override
+	public String toString() {
+		return "Client [codeClient=" + codeClient + ", nomClient=" + nomClient + ", compte=" + compte + "]";
+	}
+	
+	
+	
 
 }
